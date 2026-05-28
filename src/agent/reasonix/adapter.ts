4728fd20aa01c6b9abcb0ad7b8b9cc58b27e3b22
MYHMZ20 <MYHMZ20@users.noreply.github.com>
2026-05-29 04:11:54 +0800

    debug: add logging to reasonix binary resolution

--- src/agent/reasonix/adapter.ts
+++ src/agent/reasonix/adapter.ts
@@ -72,6 +72,7 @@ export class ReasonixAdapter implements AgentAdapter {
     this.binary = resolved.cmd;
     this.baseArgs = resolved.args;
     this.shell = resolved.shell;
+    console.log(`[reasonix-adapter] binary=${this.binary} args=${this.baseArgs.join(' ')} shell=${this.shell}`);
   }
 
   async isAvailable(): Promise<boolean> {
